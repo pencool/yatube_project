9e82673350dfe388bc940048dcc0f5abb67237d2
Павел Корчилов <pencool2@yandex.ru>
2022-09-03 16:46:48 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Привет это главная страница '
+                        'проекта Yatube, оставайся тут, скоро будет весело!')
+
+
+def group_posts(request, text):
+    return HttpResponse(f'Пока здесь видно только это: {text}')
