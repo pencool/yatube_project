04fccfee559b3d7da64d6604cf6207196ccb82da
Павел Корчилов <pencool2@yandex.ru>
2022-09-04 23:58:57 +0300

    добавлена модель Groups, изменены вью функции перделаны шаблоны под последнее задание из темы django ORM, осталось исправить ошибку, описанную в задании, пока не понятно почему она появляется, хотя все вроде как верно

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,6 +5,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group_list.html', views.group_posts, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
     #path('group/<slug:text>/', views.group_posts),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'posts': posts
     }
     return render(request, template, context)
 
 
-def group_posts(request):
+def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
-
-#HttpResponse(f'Пока здесь видно только это: {text}')
-#, text)
