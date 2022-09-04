b1d0ade4e7245b7e5fb8a2530376584b4099000f
Павел Корчилов <pencool2@yandex.ru>
2022-09-04 19:37:52 +0300

    подключена статика, поправлены стили

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group_list.html', views.group_posts),
+    path('', views.index, name='index'),
+    path('group_list.html', views.group_posts, name='group_list'),
     #path('group/<slug:text>/', views.group_posts),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,11 +5,21 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'posts/index.html')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
 
 
 def group_posts(request):
-    return render(request, 'posts/group_list.html')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
 
 #HttpResponse(f'Пока здесь видно только это: {text}')
 #, text)
